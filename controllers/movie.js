42b9e7cd158b116e1e483c56b16fa46556c30645
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-02-20 00:44:29 +0300

    Дипломная работа
    Создан контроллер и роутер для реста добавления фильмов.
    Валидация запросов celebrate.

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,11 +1,40 @@
 const Movie = require('../models/movie');
+const ValidationError = require('../errors/ValidationError');
 
 function getSavedMovies(req, res, next) {
   Movie.find({ owner: req.user._id })
-    .then((movies) => {
-      res.send(movies);
+    .then((movies) => res.send(movies))
+    .catch(next);
+}
+
+function addMovie(req, res, next) {
+  const {
+    country, director, duration, year, description, image,
+    trailerLink, nameRU, nameEN, thumbnail, movieId,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: req.user._id,
+  })
+    .then((movie) => res.send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new ValidationError(err.message.replace('movie validation failed: ', ''));
+      } else {
+        next(err);
+      }
     })
     .catch(next);
 }
 
-module.exports = { getSavedMovies };
+module.exports = { getSavedMovies, addMovie };
--- routes/movie.js
+++ routes/movie.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
-const { getSavedMovies } = require('../controllers/movie');
+const { Joi, celebrate } = require('celebrate');
+const { getSavedMovies, addMovie } = require('../controllers/movie');
 
 router.get('/movies', getSavedMovies);
+
+router.post('/movies', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().regex(/^https?:\/\/(w{3}\.)?.*$/),
+    trailerLink: Joi.string().required().regex(/^https?:\/\/(w{3}\.)?.*$/),
+    thumbnail: Joi.string().required().regex(/^https?:\/\/(w{3}\.)?.*$/),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    movieId: Joi.string().required(),
+  }),
+}), addMovie);
 
 module.exports = router;
